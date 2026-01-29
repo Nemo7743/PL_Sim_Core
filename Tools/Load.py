b2f026d499ba6f2226ec50b22b886d848dee08c3
Nemo7743 <legoabc0@gmail.com>
2026-01-29 15:00:04 +0800

    修正資料轉換器的loader

--- Tools/Load.py
+++ Tools/Load.py
@@ -2,132 +2,205 @@ import shutil
 
 # ========== DW ==========
 #DW Bias
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\bias_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\DW\bias_storage0.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\bias_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\DW\bias_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\bias_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\DW\bias_storage1.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\bias_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\DW\bias_storage1.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\bias_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\DW\bias_storage2.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\bias_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\DW\bias_storage2.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\bias_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\DW\bias_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\bias_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\DW\bias_storage3.txt'
 shutil.copy2(src_path, dst_path)
 
 #DW Weight
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\weight_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\DW\weight_storage0.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\weight_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\weight_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\weight_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\DW\weight_storage1.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\weight_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\weight_storage1.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\weight_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\DW\weight_storage2.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\weight_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\weight_storage2.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\weight_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\DW\weight_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\weight_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\weight_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#DW Fmap
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\tile_buffer1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\tile_buffer1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\tile_buffer2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\tile_buffer2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\tile_buffer3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\DW\tile_buffer3.txt'
 shutil.copy2(src_path, dst_path)
 
 #DW Output
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\DW_Sim\output.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_O\DW\output.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\DW_Sim\output.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_O\DW\output.txt'
 shutil.copy2(src_path, dst_path)
+
 
 # ========== PW ==========
 #PW Bias
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\bias_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\PW\bias_storage0.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\bias_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\PW\bias_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\bias_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\PW\bias_storage1.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\bias_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\PW\bias_storage1.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\bias_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\PW\bias_storage2.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\bias_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\PW\bias_storage2.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\bias_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\PW\bias_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\bias_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\PW\bias_storage3.txt'
 shutil.copy2(src_path, dst_path)
 
 #PW Weight
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\weight_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\PW\weight_storage0.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\weight_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\PW\weight_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\weight_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\PW\weight_storage1.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\weight_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\PW\weight_storage1.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\weight_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\PW\weight_storage2.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\weight_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\PW\weight_storage2.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\weight_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\PW\weight_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\weight_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\PW\weight_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#PW Fmap
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\tile_buffer1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\PW\tile_buffer1.txt'
 shutil.copy2(src_path, dst_path)
 
 #PW Output
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\PW_Sim\output.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_O\PW\output.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\PW_Sim\output.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_O\PW\output.txt'
 shutil.copy2(src_path, dst_path)
 
 
 # ========== FC ==========
 #FC Bias
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\bias_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\FC\bias_storage0.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\bias_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\FC\bias_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\bias_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\FC\bias_storage1.txt'
-shutil.copy2(src_path, dst_path)
-
-#PW Weight
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\weight_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\FC\weight_storage0.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\weight_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\FC\weight_storage1.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\weight_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\FC\weight_storage2.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\weight_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\FC\weight_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\bias_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\FC\bias_storage1.txt'
 shutil.copy2(src_path, dst_path)
 
-#PW Output
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\FC_Sim\output.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_O\FC\output.txt'
+#FC Weight
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\weight_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\FC\weight_storage0.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\weight_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\FC\weight_storage1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\weight_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\FC\weight_storage2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\weight_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\FC\weight_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#FC Fmap
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\tile_buffer1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\FC\tile_buffer1.txt'
+shutil.copy2(src_path, dst_path)
+
+#FC Output
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\FC_Sim\output.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_O\FC\output.txt'
 shutil.copy2(src_path, dst_path)
 
 
 # ========== ConvLast ==========
-#FC Bias
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\bias_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\ConvLast\bias_storage0.txt'
+#ConvLast Bias
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\bias_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\ConvLast\bias_storage0.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\bias_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\ConvLast\bias_storage1.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\bias_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\ConvLast\bias_storage1.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\bias_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\ConvLast\bias_storage2.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\bias_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\ConvLast\bias_storage2.txt'
 shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\bias_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_B\ConvLast\bias_storage3.txt'
-shutil.copy2(src_path, dst_path)
-
-#PW Weight
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\weight_storage0.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\ConvLast\weight_storage0.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\weight_storage1.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\ConvLast\weight_storage1.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\weight_storage2.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\ConvLast\weight_storage2.txt'
-shutil.copy2(src_path, dst_path)
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\weight_storage3.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_W\ConvLast\weight_storage3.txt'
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\bias_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\ConvLast\bias_storage3.txt'
 shutil.copy2(src_path, dst_path)
 
-#PW Output
-src_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\ConvLast_Sim\output.txt' 
-dst_path = r'C:\Users\legoa\NCU\專題\專題內容\PL_Sim_Core-main\Tools\Raw_O\ConvLast\output.txt'
+#ConvLast Weight
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\weight_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\ConvLast\weight_storage0.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\weight_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\ConvLast\weight_storage1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\weight_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\ConvLast\weight_storage2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\weight_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\ConvLast\weight_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#ConvLast Fmap
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\tile_buffer1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\ConvLast\tile_buffer1.txt'
+shutil.copy2(src_path, dst_path)
+
+#ConvLast Output
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\ConvLast_Sim\output.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_O\ConvLast\output.txt'
+shutil.copy2(src_path, dst_path)
+
+
+# ========== Conv1 ==========
+#Conv1 Bias
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\bias_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\Conv1\bias_storage0.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\bias_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\Conv1\bias_storage1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\bias_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\Conv1\bias_storage2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\bias_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_B\Conv1\bias_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#Conv1 Weight
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\weight_storage0.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\weight_storage0.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\weight_storage1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\weight_storage1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\weight_storage2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\weight_storage2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\weight_storage3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\weight_storage3.txt'
+shutil.copy2(src_path, dst_path)
+
+#Conv1 Fmap
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\tile_buffer1.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\tile_buffer1.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\tile_buffer2.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\tile_buffer2.txt'
+shutil.copy2(src_path, dst_path)
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\tile_buffer3.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_W\Conv1\tile_buffer3.txt'
+shutil.copy2(src_path, dst_path)
+
+#Conv1 Output
+src_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Conv1_Sim\output.txt' 
+dst_path = r'C:\Users\legoa\NCU\專題\專題內容\硬體模擬\PL_Sim_Core\Tools\Raw_O\Conv1\output.txt'
 shutil.copy2(src_path, dst_path)
